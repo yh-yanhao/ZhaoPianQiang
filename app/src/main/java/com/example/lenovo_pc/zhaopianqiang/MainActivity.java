e0b654bd4c19b59f35e6bae0df8ac8755ed225e7
yanhao <707668941@qq.com>
2016-12-13 22:14:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lenovo_pc/zhaopianqiang/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.lenovo_pc.zhaopianqiang;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewTreeObserver;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+    /**
+     * 用于展示照片墙的GridView
+     */
+    private GridView mPhotoWall;
+
+    /**
+     * GridView的适配器
+     */
+    private PhotoWallAdapter mAdapter;
+
+    private int mImageThumbSize;
+    private int mImageThumbSpacing;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mImageThumbSize = getResources().getDimensionPixelSize(
+                R.dimen.image_thumbnail_size);
+        mImageThumbSpacing = getResources().getDimensionPixelSize(
+                R.dimen.image_thumbnail_size);
+        mPhotoWall = (GridView) findViewById(R.id.photo_wall);
+        mAdapter = new PhotoWallAdapter(this, 0, Images.imageThumbUrls,
+                mPhotoWall);
+        mPhotoWall.setAdapter(mAdapter);
+        mPhotoWall.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+
+                    @Override
+                    public void onGlobalLayout() {
+                        final int numColumns = (int) Math.floor(mPhotoWall
+                                .getWidth()
+                                / (mImageThumbSize + mImageThumbSpacing));
+                        if (numColumns > 0) {
+                            int columnWidth = (mPhotoWall.getWidth() / numColumns)
+                                    - mImageThumbSpacing;
+                            mAdapter.setItemHeight(columnWidth);
+                            mPhotoWall.getViewTreeObserver()
+                                    .removeGlobalOnLayoutListener(this);
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mAdapter.fluchCache();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 退出程序时结束所有的下载任务
+        mAdapter.cancelAllTasks();
+    }
+}
+
